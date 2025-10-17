ec9be9329cb5d14827cc01b3bc85415aabc0520a
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-10-16 22:28:42 -0300

    update service worker cache revision for index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f8b0af05'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oud6378gcj"
+    "revision": "0.3j7l0f8qvr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
